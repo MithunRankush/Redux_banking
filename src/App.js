1d1895e66f447958725666c56b7b4ec1cf478a52
Mithun R Ankush <121188000+MithunRankush@users.noreply.github.com>
2023-05-24 17:17:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Bank from './components/Bank';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+     <Navbar></Navbar>
+    <div className="container">
+       <Bank></Bank>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Bank.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state";
+
+const Bank = () => {
+
+    const dispatch = useDispatch();
+    const {depositMoney,withdrawMoney} = bindActionCreators(actionCreators,dispatch)
+    return ( <div>
+        <h1>Welcome, May i help you</h1>
+        <h2>Deposit/Withdraw Money</h2>
+        <button className="btn btn-primary mx-2" onClick={()=>depositMoney(100)}>+</button>
+          Update amount
+        <button className="btn btn-primary mx-2" onClick={()=>withdrawMoney(100)}>-</button>
+        {/* <button className="btn btn-primary mx-2" onClick={()=>dispatch(actionCreators.depositMoney(100))}>+</button>
+          Update amount
+        <button className="btn btn-primary mx-2" onClick={()=>dispatch(actionCreators.withdrawMoney(100))}>-</button> */}
+         {/* <button className="btn btn-primary mx-2" onClick={()=>actions.depositMoney(100)}>+</button>
+          Update amount
+        <button className="btn btn-primary mx-2" onClick={()=>actions.withdrawMoney(100)}>-</button> */}
+    </div> );
+}
+ 
+export default Bank;
